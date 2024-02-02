9d7663e5e670c93514eea31b574c6e668dacd15f
Etarley Taveras Taveras <etarley.taveras@luckettinc.com>
2024-02-02 17:16:29 -0400

    refactor: integrate ProductsCarousel, handle additional product fetching
    Imported ProductsCarousel component for displaying additional products.
    Created getMoreProducts async function to fetch more products from 'https://fakestoreapi.com/products?limit=14'.
    Updated the Page component to fetch additional products and display them using ProductsCarousel.
    Enhanced conditional rendering for product details to handle potential null values.
    Adjusted styling and layout for better UI presentation.
    Minor code cleanup and organization.
    chore: update defaultTheme to light in ThemeProvider in RootLayout
    
    Changed the defaultTheme value from system to light in the ThemeProvider component within the RootLayout of the application. This modification aims to provide a consistent light theme experience for users, deviating from the previous default of the system theme.

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,4 +1,5 @@
 // 'use client'
+import ProductsCarrousel from '@/components/ProductsCarousel';
 import { Button } from '@/components/ui/button';
 import { Separator } from '@/components/ui/separator';
 // import { useQuery } from '@tanstack/react-query';
@@ -31,6 +32,15 @@ async function getProduct(productId: string) {
   return res.json();
 }
 
+async function getMoreProducts() {
+  const res = await fetch ('https://fakestoreapi.com/products?limit=14')
+
+  if (!res.ok){
+    throw new Error('Failed to fetch data')
+  }
+  return res.json() 
+}
+
 const Page: React.FC<{ params: { id: string } }> = async ({ params }) => {
   // const { data, isLoading, isError } = useQuery(
   //   {
@@ -48,23 +58,30 @@ const Page: React.FC<{ params: { id: string } }> = async ({ params }) => {
   // }
 
   const product = await getProduct(params.id) as Product;
+  const allProducts= await getMoreProducts()
 
   return (
     <div className='grid sm:my-4 sm:grid-cols-2 sm:gap-1'>
-      <p className="text-neutral-500 sm:col-span-2">categories<ChevronRight className='inline size-4'/>{product.category}</p>
-      <Image src={product.image} alt={product.title} className="mr-4 rounded border object-contain p-2 sm:size-[36rem]" width={1500} height={1500} />
-      <div className='flex flex-col gap-1'>
-        <h1 className="mb-0.5 text-2xl font-bold sm:text-3xl">{product.title}</h1>
+      <p className="text-neutral-500 sm:col-span-2">categories<ChevronRight className='inline size-4'/>{product?.category}</p>
+      <Image src={product?.image} alt={product?.title} className="mr-4 size-[30rem] rounded border object-contain p-2 sm:size-[36rem]" width={1500} height={1500} />
+      <div className='flex flex-col'>
+        <h1 className="mb-0.5 text-2xl font-bold sm:text-3xl">{product?.title}</h1>
         {/* <div className='flex flex-col gap-2'>     */}
          <Separator />
-        <p className="text-2xl font-semibold text-neutral-800">$ {product.price}</p>
+        <p className="my-2 text-2xl font-light text-neutral-800 dark:text-neutral-200">$ {product?.price}</p>
         <Button>Add to Cart <PlusIcon className='ml-2 size-4' /></Button>
         {/* </div> */}
       </div>
-<div className='mt-6 sm:col-span-2'>
+<div className='mt-4 sm:col-span-2 sm:mt-6'>
           <h2 className='text-2xl font-semibold'>Description</h2>
-          <p className="mt-4 text-sm">{product.description}</p>
+          <p className="mt-2 text-sm">{product?.description}</p>
 </div>
+
+<div className='mt-6 sm:col-span-2 sm:mt-8'>
+  <Separator />
+  <ProductsCarrousel products={allProducts}/>
+</div>
+
     </div>
   );
 };
---
+++ src/components/ProductsCarousel.tsx
@@ -0,0 +1,64 @@
+'use client'
+import Autoplay from 'embla-carousel-autoplay';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Card, CardContent, CardTitle } from './ui/card';
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel';
+
+
+interface Product {
+  id: number;
+  title: string;
+  price: string;
+  category: string;
+  description: string;
+  image: string;
+}
+
+const ProductsCarrousel= ({products} : {products: Product[]}) => {
+  return (
+    <Carousel
+    opts={
+        {
+            loop:true
+        }
+    }
+    plugins={[
+        Autoplay({
+          delay: 6000,
+          stopOnFocusIn:true,
+          stopOnMouseEnter:true
+        }),
+      ]}
+      className='container max-w-xs p-0 sm:max-w-none'
+    >
+    <CarouselContent className='justify-between py-6'>
+         {products.map((product) => (
+          <CarouselItem key={product.id} className='ml-4 md:basis-1/2 lg:basis-1/5'>
+            <Card className='group transition-transform duration-300 ease-in-out hover:scale-105'>
+              <CardContent className='items-center'>
+                <Link href={`/products/${product.id}`}>
+                  
+                    <Image
+                      src={product.image}
+                      alt={product.title}
+                      width={300}
+                      height={300}
+                      className='size-[20rem] object-contain p-2'
+                    />
+                    
+                    <CardTitle className='truncate text-base group-hover:underline'>{product.title}</CardTitle>
+                    <p>{`$${product.price}`}</p>
+                </Link>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className='-left-8 top-1/2 -translate-y-1/2' />
+  <CarouselNext className='-right-8 top-1/2 -translate-y-1/2'/>
+    </Carousel>
+  )
+}
+
+export default ProductsCarrousel;

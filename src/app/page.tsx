2ba064dec96b3900b4a117a629cadc09de6e2443
Etarley Taveras Taveras <etarley.taveras@luckettinc.com>
2024-02-03 11:17:54 -0400

    feat: implement pagination in Products component
    Introduce pagination functionality in the Products component to enhance the user experience. The following changes were made:
    
    - Imported Pagination components from @/components/ui/pagination.
    - Utilized useSearchParams from next/navigation to dynamically update the page parameter in the URL.
    - Created a Pagination component with Previous and Next links, allowing users to navigate between different pages.
    - Adjusted the rendering logic in the Products component to display a subset of products based on the active page.
    - Improved styling for a responsive grid layout to showcase the paginated product cards.
    
    This commit addresses the need for a more user-friendly product listing by introducing pagination, making it easier for users to explore a large number of products.
    chore: update dependencies to latest versions
    
    Update dependencies to the latest versions:
    - @trpc/react-query: from 10.45.0 to 11.0.0-next-beta.260
    - @tanstack/react-query: from 4.18.0 to 5.18.1
    - @trpc/client: from 10.45.0 to 11.0.0-next-beta.260
    - @trpc/server: from 10.45.0 to 11.0.0-next-beta.260
    
    Peer dependencies have also been updated:
    - @tanstack/react-query: ^4.18.0 → ^5.18.1
    
    Additionally, the resolution field has been updated with the latest integrity hashes.
    
    This commit reflects the need to stay up-to-date with the latest versions and peer dependencies, ensuring compatibility and taking advantage of potential improvements and bug fixes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,20 @@
 
 import Categories from "@/components/Categories";
 import Products from "@/components/products/products";
-import { getUserAuth } from "@/lib/auth/utils";
 
 
 export default async function Home() {
   
-  const { session } = await getUserAuth();
+  // const { session } = await getUserAuth();
   return (
       <>
     <Categories/>
     <Products/>
-        {session ? (
+        {/* {session ? (
           <pre className="whitespace-break-spaces break-all rounded-sm bg-secondary p-4 text-secondary-foreground shadow-sm">
             {JSON.stringify(session, null, 2)}
           </pre>
-        ) : null}
+        ) : null} */}
       </>
   );
 }
--- src/components/products/products.tsx
+++ src/components/products/products.tsx
@@ -1,12 +1,23 @@
-// 'use client'
+'use client'
 import { Button } from '@/components/ui/button';
-// import { useQuery } from '@tanstack/react-query';
-// import axios from 'axios';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious
+} from "@/components/ui/pagination";
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import { PlusIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { Card, CardContent, CardFooter } from '../ui/card';
+
+import { useSearchParams } from 'next/navigation';
+ 
 
 interface Product {
   id: number;
@@ -17,37 +28,57 @@ interface Product {
   image: string;
 }
 
-// const fetchProducts = async () => {
-//   const response = await axios.get<Product[]>('https://fakestoreapi.com/products?limit=8');
-//   return response.data;
-// };
+const fetchProducts = async () => {
+  const response = await axios.get<Product[]>(`https://fakestoreapi.com/products`);
+  return response.data;
+};
 
-async function getData() {
-  const res = await fetch ('https://fakestoreapi.com/products?limit=8')
+// async function getData() {
+//   const res = await fetch ('https://fakestoreapi.com/products')
 
-  if (!res.ok){
-    throw new Error('Failed to fetch data')
+//   if (!res.ok){
+//     throw new Error('Failed to fetch data')
+//   }
+//   return res.json() 
+// }
+
+const Products: React.FC = () => {
+
+  const activePage = parseInt(useSearchParams().get('page')||'1')
+  
+  
+    const { data, isLoading, isError } = useQuery({
+    queryKey: ['products'],
+    queryFn: fetchProducts,
+  });
+
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
-  return res.json() 
-}
 
-const Products: React.FC = async () => {
-  // const { data, isLoading, isError } = useQuery({queryKey:['products'], queryFn:fetchProducts});
+  if (isError) {
+    return <div>Error fetching data</div>;
+  }
 
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
+  if(!data){
+    return
+  }
 
-  // if (isError) {
-  //   return <div>Error fetching data</div>;
-  // }
+   const itemsPerPage = 8;
+  const startIdx = (activePage - 1) * itemsPerPage;
+  const endIdx = startIdx + itemsPerPage;
+  const currentData = data.slice(startIdx, endIdx);
 
-  const data = await getData() as Product[]
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+  const pages = Array.from({ length: pageCount }, (_, index) => index + 1);
+
 
 
   return (
+    <div>
     <div className="my-6 grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      {data?.map((product) => (
+      {currentData?.map((product) => (
         <Card key={product.id} className="rounded-lg shadow-lg">
          <CardContent>
 <Link href={`products/${product.id}`}>
@@ -61,6 +92,28 @@ const Products: React.FC = async () => {
          </CardFooter>
         </Card>
       ))}
+    </div>
+  <Pagination className='mb-6'>
+      <PaginationContent>
+        <PaginationItem>
+          {activePage > 1 && <PaginationPrevious href={`?page=${activePage-1}`} />}
+        </PaginationItem>
+        {
+          pages.map(page=>(
+            <PaginationItem key={page}>
+              {activePage ===page ? <PaginationLink href={`?page=${page}`} isActive>{page}</PaginationLink>:
+               <PaginationLink href={`?page=${page}`}>{page}</PaginationLink>
+              }
+             
+              
+            </PaginationItem>
+          ))
+        }
+        <PaginationItem>
+          {activePage < pages.length && <PaginationNext href={`?page=${activePage+1}`} />}
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
     </div>
   );
 };
